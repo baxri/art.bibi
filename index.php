edf144c316ea481568263d9545c4ef29fcd82b67
Giorgi <giorgi.bibilashvili89@gmail.com>
2018-03-25 23:47:55 +0400

    --

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         </a>
         <ul class="right hide-on-med-and-down">
             <li><a href="javascrit:void(0)">Mob: +995 555 73 78 00</a></li>
-            <li><a href="javascrit:void(0)">Email: giorgi.bibiashvili89@gmail.com</a></li>
+            <li><a href="javascrit:void(0)">Email: mr.artbibi@gmail.com</a></li>
         </ul>
 
 <!--        <ul id="nav-mobile" class="side-nav">-->
